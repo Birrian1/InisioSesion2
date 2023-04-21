36576b94abf13c3d39fd98482760a317e6660102
DESKTOP-UCNGISC\BCJL1 <91810728+Birrian1@users.noreply.github.com>
2023-04-20 23:27:16 -0700

    Initial commit

---
+++ app/src/main/java/brian/jimenez/inisiosesion2/PrincipalActivity.kt
@@ -0,0 +1,31 @@
+package brian.jimenez.inisiosesion2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class PrincipalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_principal)
+
+        val bundle = intent.extras
+        val tv_nombre: TextView = findViewById(R.id.tv_nombre)
+        val tv_email: TextView = findViewById(R.id.tv_email)
+
+        val btn_cerrar: Button = findViewById(R.id.btn_cerrar)
+
+
+        if (bundle != null) {
+            val name = bundle.getString("name")
+            val email = bundle.getString("email")
+            tv_nombre.setText(name)
+            tv_email.setText(email)
+        }
+
+        btn_cerrar.setOnClickListener{
+            finish()
+        }
+    }
+}
